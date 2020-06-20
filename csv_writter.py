797e48007b860e77092babdd5408edd9ae466ad6
Jan Polák <polakjan101@gmail.com>
2020-06-20 17:11:36 +0200

    refactor, add exception handling

--- csv_writter.py
+++ csv_writter.py
@@ -1,29 +1,14 @@
 import csv
+import sys
 
 def write_csv(file, data):
-    with open(file, "w", newline="") as csv_file:
+    with open(file + ".csv", "w", newline="") as csv_file:
 
         header = data[0].keys()
         writer = csv.DictWriter(csv_file, fieldnames=header)
-        writer.writeheader()
 
-        writer.writerows(data)
-
-# f = open("sample.csv", "w")
-# writer = csv.DictWriter(
-#     f, fieldnames=["fruit", "count"])
-# writer.writeheader()
-# writer.writerow({"fruit": "apple", "count": "1"})
-# writer.writerow({"fruit": "banana", "count": "2"})
-
-
-# writer.writerows(
-#     [{"fruit": "apple", "count": "1"},
-#     {"fruit": "banana", "count": "2"}])
-# f.close()
-# Output
-# sample.csv
-# fruit,count
-# apple,1
-# banana,2
-
+        try:
+            writer.writeheader()
+            writer.writerows(data)
+        except csv.Error as e:
+            sys.exit(f"CSV Error: {e}")
